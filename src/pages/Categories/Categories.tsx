1b2a1e678081811ccaffaa23e6201b6a68b0629b
Tyan_Pavel <ptyan_1@edu.hse.ru>
2025-01-26 12:54:23 +0300

    feat: update Categories component
    Add category list

--- src/pages/Categories/Categories.tsx
+++ src/pages/Categories/Categories.tsx
@@ -1,10 +1,26 @@
+import { MUIStyles } from "@/@types";
+import { CategoryCard } from "@/components/CategoryCard/CategoryCard";
 import { useCategories } from "@/hooks/useCategories";
+import { Box } from "@mui/material";
 
 const Categories = () => {
   const { categories, addCategoryByName, deleteCategoryByName } =
     useCategories();
 
-  return <div>Categories</div>;
+  const wrapperStyles: MUIStyles = {
+    display: "grid",
+    gap: "20px 30px",
+    gridTemplateColumns: "repeat(4, 1fr)",
+    marginTop: "30px",
+  };
+
+  return (
+    <Box sx={wrapperStyles}>
+      {categories.map((category) => (
+        <CategoryCard key={category.name}>{category.name}</CategoryCard>
+      ))}
+    </Box>
+  );
 };
 
 export default Categories;
