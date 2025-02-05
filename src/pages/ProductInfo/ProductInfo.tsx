e8bd2749efb9fbba4e57ff16ef5797942ea494a6
Tyan_Pavel <ptyan_1@edu.hse.ru>
2025-02-05 21:52:56 +0300

    feat: add form for adding new product

--- src/pages/ProductInfo/ProductInfo.tsx
+++ src/pages/ProductInfo/ProductInfo.tsx
@@ -1,10 +1,27 @@
 import { MUIStyles } from "@/@types";
 import { HorizontalDivider } from "@/components/HorizontalDivider/HorizontalDivider";
+import { EMPTY_INPUT_ERROR_MESSAGE } from "@/constants";
 import { useProductActions } from "@/hooks/useProductActions";
 import { Layout } from "@/layouts/Layout";
 import { useAppSelector } from "@/redux/hooks";
-import { Box, Button, Card, CardMedia, Typography } from "@mui/material";
-import { useParams } from "react-router";
+import {
+  Box,
+  Button,
+  Card,
+  CardMedia,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router";
 
 const ProductInfo = () => {
   const containerStyles: MUIStyles = {
@@ -58,17 +75,156 @@ const ProductInfo = () => {
     textWrap: "balance",
   };
 
+  const buttonContainerStyles: MUIStyles = {
+    display: "flex",
+    gap: "20px",
+  };
+
   const { id } = useParams();
 
   const currentProduct = useAppSelector((state) =>
     state.products.productList.find((product) => product.id === id)
   );
 
-  const { addProduct, deleteProductById, updateProduct } = useProductActions();
+  const { deleteProductById, updateProduct } = useProductActions();
+
+  const navigate = useNavigate();
+  const deleteCurrentProduct = () => {
+    deleteProductById(currentProduct?.id || "");
+    navigate("/");
+  };
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const [name, setName] = useState<string>(currentProduct?.name || "");
+
+  const categories = useAppSelector((state) => state.categories.categoryList);
+  const [category, setCategory] = useState<string>(
+    currentProduct?.category || ""
+  );
+  const [description, setDescription] = useState<string>(
+    currentProduct?.description || ""
+  );
+  const [measure, setMeasure] = useState<string>(currentProduct?.measure || "");
+  const [count, setCount] = useState<number>(currentProduct?.count || 0);
+
+  const onChangeName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
+  const onChangeCategory = (event: SelectChangeEvent<string>) => {
+    setCategory(event.target.value);
+  };
+
+  const onChangeDescription = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDescription(event.target.value);
+  };
+
+  const onChangeCount = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCount(Number(event.target.value));
+  };
+
+  const onChangeMeasure = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMeasure(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    updateProduct({
+      id: currentProduct?.id || "",
+      img: currentProduct?.img,
+      name,
+      category,
+      description,
+      measure,
+      count,
+    });
+
+    setIsOpen(false);
+  };
 
   return (
     <Layout hasDrawer={false}>
       <Box sx={containerStyles}>
+        <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
+          <DialogContent>
+            <DialogTitle>Изменить информацию о продукте</DialogTitle>
+            <TextField
+              required
+              margin="dense"
+              id="name"
+              name="name"
+              label="Имя"
+              type="text"
+              fullWidth
+              variant="standard"
+              value={name}
+              onChange={onChangeName}
+              error={!name}
+              helperText={!name && EMPTY_INPUT_ERROR_MESSAGE}
+            />
+            <InputLabel id="category">Категория</InputLabel>
+            <Select
+              labelId="category"
+              id="category"
+              value={category}
+              label="Age"
+              onChange={onChangeCategory}
+            >
+              {categories.map((category) => (
+                <MenuItem key={category} value={category}>
+                  {category}
+                </MenuItem>
+              ))}
+            </Select>
+            <TextField
+              required
+              margin="dense"
+              id="description"
+              name="description"
+              label="Описание"
+              type="text"
+              fullWidth
+              variant="standard"
+              value={description}
+              onChange={onChangeDescription}
+              error={!description}
+              helperText={!description && EMPTY_INPUT_ERROR_MESSAGE}
+            />
+            <TextField
+              required
+              margin="dense"
+              id="measure"
+              name="measure"
+              label="Единица количества"
+              type="text"
+              fullWidth
+              variant="standard"
+              value={measure}
+              onChange={onChangeMeasure}
+              error={!measure}
+              helperText={!measure && EMPTY_INPUT_ERROR_MESSAGE}
+            />
+            <TextField
+              required
+              margin="dense"
+              id="count"
+              name="count"
+              label="Количество"
+              type="number"
+              fullWidth
+              variant="standard"
+              value={count}
+              onChange={onChangeCount}
+              error={!count}
+              helperText={!count && EMPTY_INPUT_ERROR_MESSAGE}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button type="submit" onClick={handleSubmit}>
+              Подтвердить
+            </Button>
+          </DialogActions>
+        </Dialog>
         <Card sx={cardStyles}>
           {currentProduct?.img && (
             <CardMedia
@@ -103,7 +259,7 @@ const ProductInfo = () => {
             </Typography>
             <HorizontalDivider />
             <Typography sx={titleStyles} variant="h1">
-              {"Категория: " + currentProduct?.category}
+              {"Единица количества: " + currentProduct?.measure}
             </Typography>
             <HorizontalDivider />
             <Typography sx={titleStyles} variant="h1">
@@ -114,7 +270,12 @@ const ProductInfo = () => {
             </Typography>
           </Box>
         </Card>
-        <Button>Редактировать информацию</Button>
+        <Box sx={buttonContainerStyles}>
+          <Button onClick={() => setIsOpen(true)}>
+            Редактировать информацию
+          </Button>
+          <Button onClick={deleteCurrentProduct}>Удалить продукт</Button>
+        </Box>
       </Box>
     </Layout>
   );
