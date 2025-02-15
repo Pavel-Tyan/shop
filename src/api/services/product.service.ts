7a2fb6c152ec949ed584c779ab004306f225d7b4
Tyan_Pavel <paveltyan032@gmail.com>
2025-02-15 12:39:29 +0300

    feat: add product service for api

---
+++ src/api/services/product.service.ts
@@ -0,0 +1,35 @@
+import { ProductDto } from "../@types/api";
+import { api } from "../instance";
+
+class ProductService {
+  private static readonly URL_SEGMENT = "/products";
+
+  async getProducts(limit: number, offset: number) {
+    const res = await api.get(
+      `${ProductService.URL_SEGMENT}?limit=${limit}&offset=${offset}`
+    );
+    return res.data;
+  }
+
+  async getProductById(id: number) {
+    const res = await api.get(`${ProductService.URL_SEGMENT}/${id}`);
+    return res.data;
+  }
+
+  async postProduct({ params, config }: AxiosRequestConfig<ProductDto>) {
+    const res = await api.post(ProductService.URL_SEGMENT, params, config);
+    return res.data;
+  }
+
+  async updateProduct({ params, config }: AxiosRequestConfig<ProductDto>) {
+    const res = await api.patch(ProductService.URL_SEGMENT, params, config);
+    return res.data;
+  }
+
+  async deleteProductById(id: number) {
+    const res = await api.delete(`${ProductService.URL_SEGMENT}/${id}`);
+    return res.data;
+  }
+}
+
+export default new ProductService();
