e09b878f0e1c0947768e9ad020f683dfb4980ce9
Tyan_Pavel <paveltyan032@gmail.com>
2025-02-24 00:17:19 +0300

    feat: add constant for pagination

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
 export const EMPTY_INPUT_ERROR_MESSAGE = "Поле не может быть пустым";
 export const NO_CATEGORY_FILTER_MESSAGE = "Без фильтра";
 export const CARDS_PER_PAGE = 8;
+export const PRODUCT_LIMIT = 30;
